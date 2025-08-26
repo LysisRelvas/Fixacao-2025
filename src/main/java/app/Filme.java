706ab364a3e7ae5bc0337eebb9c1fff6cbce491c
Lysis Relvas <lysisrelvas789@gmail.com>
2025-08-26 00:40:47 +0000

    Aula 25/08/2025

---
+++ src/main/java/app/Filme.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Filme {
+    private long id;
+    private String titulo;
+    private Genero genero
+
+
+}
--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -3,6 +3,12 @@ package app;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!");
+        Genero g = new Genero(1, "Aventura");
+
+        Genero g2 = new Genero(0,"");
+       
+        System.out.println(g.getNome());
+        System.out.println(g2.getId());
     }
 }
 
